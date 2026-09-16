5eee67c0251b20941065dc9f93f85d363e7b5522
mohammad ghf <mohammad.mahdi.ghf.pc@gmail.com>
2026-09-16 18:42:44 +0330

    fix style

--- src/features/todos/components/TodoForm.tsx
+++ src/features/todos/components/TodoForm.tsx
@@ -30,7 +30,7 @@ const TodoForm = ({
 
       <div className="flex gap-1">
         <select
-          className="bg-gray-700 dark:bg-purple-800 text-white cursor-pointer py-3 dark:hover:bg-purple-900 text-sm rounded-md outline-none"
+          className="bg-gray-700 dark:bg-purple-800 text-white cursor-pointer py-3 px-1 dark:hover:bg-purple-900 text-sm rounded-md outline-none"
           value={priority}
           onChange={(e) => setPriority(e.target.value as Priority)}
         >
@@ -42,7 +42,7 @@ const TodoForm = ({
         <select
           onChange={(e) => setFilter(e.target.value as Filter)}
           value={filter}
-          className="bg-gray-700 dark:bg-purple-800 dark:hover:bg-purple-900 text-white rounded-md cursor-pointer outline-none text-sm py-3"
+          className="bg-gray-700 dark:bg-purple-800 dark:hover:bg-purple-900 text-white rounded-md cursor-pointer outline-none text-sm py-3 px-1"
         >
           <option className="cursor-pointer" value="all">
             all
