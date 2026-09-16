73fe8f5d9a3bc58bfefdfffc273ea01917f3be96
mohammad ghf <mohammad.mahdi.ghf.pc@gmail.com>
2026-09-16 18:41:38 +0330

    fix style

--- src/features/todos/components/TodoForm.tsx
+++ src/features/todos/components/TodoForm.tsx
@@ -28,31 +28,33 @@ const TodoForm = ({
         placeholder="Add Task Name"
       />
 
-      <select
-        className="bg-gray-700 dark:bg-purple-800 text-white cursor-pointer py-3 dark:hover:bg-purple-900 text-sm rounded-md outline-none"
-        value={priority}
-        onChange={(e) => setPriority(e.target.value as Priority)}
-      >
-        <option value="low">low</option>
-        <option value="medium">medium</option>
-        <option value="high">high</option>
-      </select>
+      <div className="flex gap-1">
+        <select
+          className="bg-gray-700 dark:bg-purple-800 text-white cursor-pointer py-3 dark:hover:bg-purple-900 text-sm rounded-md outline-none"
+          value={priority}
+          onChange={(e) => setPriority(e.target.value as Priority)}
+        >
+          <option value="low">low</option>
+          <option value="medium">medium</option>
+          <option value="high">high</option>
+        </select>
 
-      <select
-        onChange={(e) => setFilter(e.target.value as Filter)}
-        value={filter}
-        className="bg-gray-700 dark:bg-purple-800 dark:hover:bg-purple-900 text-white rounded-md cursor-pointer outline-none text-sm"
-      >
-        <option className="cursor-pointer" value="all">
-          all
-        </option>
-        <option className="cursor-pointer" value="active">
-          active
-        </option>
-        <option className="cursor-pointer" value="completed">
-          completed
-        </option>
-      </select>
+        <select
+          onChange={(e) => setFilter(e.target.value as Filter)}
+          value={filter}
+          className="bg-gray-700 dark:bg-purple-800 dark:hover:bg-purple-900 text-white rounded-md cursor-pointer outline-none text-sm py-3"
+        >
+          <option className="cursor-pointer" value="all">
+            all
+          </option>
+          <option className="cursor-pointer" value="active">
+            active
+          </option>
+          <option className="cursor-pointer" value="completed">
+            completed
+          </option>
+        </select>
+      </div>
     </div>
   );
 };
